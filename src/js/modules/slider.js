21adc85a9d216bc86f4ff429baa2c54695036d7a
dchaika <dchaika1990@gmail.com>
2020-12-02 17:42:24 +0200

    add feature to slider

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -10,6 +10,21 @@ export default class Slider {
         if (n > this.slides.length) this.slideIndex = 1;
         if (n < 1) this.slideIndex = this.slides.length;
 
+        try {
+            this.hanson.style.opacity = '0';
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000)
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch (e) {
+            throw new Error(e)
+        }
+
         this.slides.forEach(slide => {
             slide.style.display = 'none';
         })
@@ -22,6 +37,12 @@ export default class Slider {
     }
 
     render() {
+        try {
+            this.hanson = document.querySelector('.hanson')
+        } catch (e) {
+            throw new Error(e)
+        }
+
         this.btns.forEach(btn => {
             btn.addEventListener('click', () => {
                 this.plusSlides(1)
